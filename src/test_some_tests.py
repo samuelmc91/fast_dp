56dee6b4e8f8eb78f785285dc2644f989f06ff51
Jorge Diaz <jorge7soccer@gmail.com>
2019-04-02 15:05:55 -0400

    ENH: Pythonized this file.

--- src/test_some_tests.py
+++ src/test_some_tests.py
@@ -31,30 +31,32 @@ import libtbx.load_env
 import os
 import pytest
 
+
 def test_fast_dp_X4_wide(tmpdir):
 
-  src = os.path.join(libtbx.env.under_build('xia2_regression'),
-                     'test_data', 'X4_wide')
-  dls = '/dls/science/groups/scisoft/DIALS/repositories/current/' + \
-    'xia2_regression_data/test_data/X4_wide'
-  if not os.path.exists(src):
-    if not os.path.exists(dls):
-      pytest.skip('Could not find test image file')
-    src = dls
+    src = os.path.join(libtbx.env.under_build('xia2_regression'),
+                       'test_data', 'X4_wide')
+    dls = '/dls/science/groups/scisoft/DIALS/repositories/current/' + \
+          'xia2_regression_data/test_data/X4_wide'
+    if not os.path.exists(src):
+        if not os.path.exists(dls):
+            pytest.skip('Could not find test image file')
+        src = dls
 
-  image = os.path.join(src, 'X4_wide_M1S4_2_0001.cbf')
+    image = os.path.join(src, 'X4_wide_M1S4_2_0001.cbf')
 
-  bin = os.path.split(__file__)[0].replace('src', 'bin')
-  fast_dp = os.path.join(bin, 'fast_dp')
+    bin = os.path.split(__file__)[0].replace('src', 'bin')
+    fast_dp = os.path.join(bin, 'fast_dp')
 
-  cmd = [ fast_dp, '-a', 'Ba', image ]
-  with tmpdir.as_cwd():
-    result = run_process(cmd)
+    cmd = [fast_dp, '-a', 'Ba', image]
+    with tmpdir.as_cwd():
+        result = run_process(cmd)
 
-  assert result['stderr'] == '', 'fast_dp output to STDERR:'
-  assert result['exitcode'] == 0, 'fast_dp non-zero exit code'
-  for output in ['fast_dp.mtz', 'fast_dp.log']:
-    assert tmpdir.join(output).check(), 'No output found'
+    assert result['stderr'] == '', 'fast_dp output to STDERR:'
+    assert result['exitcode'] == 0, 'fast_dp non-zero exit code'
+    for output in ['fast_dp.mtz', 'fast_dp.log']:
+        assert tmpdir.join(output).check(), 'No output found'
+
 
 def test_are_there_any_real_tests():
-  assert True, "So test! Much happy!"
+    assert True, "So test! Much happy!"
